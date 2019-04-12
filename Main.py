4d69df5ec79f51c3f605bd740e7e1c73f11e1e90
Max2191 <maxtucker2191@gmail.com>
2019-04-12 21:08:53 +0100

    Program created

--- Main.py
+++ Main.py
@@ -1,25 +1,52 @@
-# This program uses the pyplot module to plot a set of 
-# numbers (obtained from y1=x**2-x+3) and a function 
-
-import numpy as np
 import matplotlib.pyplot as plt
+from math import *
 
-# Define the array of independent variable values
-x=np.linspace(-10,10,20)
-# Define the array of dependent variable values
-y1=np.array([113.0,92.0027700831,73.2216066482, \
-56.6565096953,42.3074792244,30.1745152355,20.2576177285, \
-12.5567867036,7.07202216066,3.80332409972,2.75069252078,  \
-3.91412742382,7.29362880886,12.8891966759,20.7008310249, \
-30.728531856,42.972299169,57.432132964,74.108033241,93.0],float)
+#initiate an empy list that will store the provided isotopes
+isotopes=[]
 
-# Define the second array using a mathematical function 
-y2=0.05*x**3+20
+#begin a loop
+#prompt user to input an isotop from the list and then enter its halflife, entering the string 'stop' will kill this process
+while True:
+ isotope = input("Please provide an isotope from the list. Once you have finished entering isotopes, type 'stop'")
+ if isotope=="stop":
+    break
+ else:
+    halflife = float(input("Please provide the isotope's halflife in seconds"))
 
-print "x=", x
+#prompt user for initial amount of isotope and the elapsed time 
+    a_grams = float(input("Please provide the initial mass of the isotope in grams"))
+    e_time = float(input("Please provide the elapsed time in seconds"))
 
-# Plot both arrays 
-plt.plot(x,y1)
-plt.plot(x,y2)
-plt.title ('An example of a plot')
-plt.show()
+#add the provided isotope to the list
+    isotopes.append(isotope)
+
+#calculates the amount of isotope remaining after e_time has elapsed and assigns it to the variable i_remaining 
+
+    i_remaining=a_grams*np.exp(-log(2)*(e_time/halflife))
+    
+#calculates the initial amount of the isotope, i_req, necessry for 5g to be remaining after 30s has elapsed
+
+    i_req = 5/(np.exp(-log(2)*(30/halflife)))
+    print "the amount of ", isotope,"remaining after ", e_time," seconds is", i_remaining
+    print "the initial mass of ", isotope,"to ensure 5 g remains after half a minute is", i_req
+    
+#defines the array of independent variable on the x-axis
+    X=np.linspace(0,11,11)
+   
+#defines the array of dependent variable on the y-axis
+    Y=a_grams*np.exp(-log(2)*(X/halflife))
+
+#creates a plot with these parameters
+
+    plt.plot(X,Y)
+    plt.title ('Plot of isotopes')
+    plt.xlabel("Time / s")
+    plt.ylabel("Amount / g")
+
+#renders a plot for all the entered isotopes
+    plt.show()
+
+#print the list of isotopes studies (blank spaces are aesthetic only)
+    print ""
+    print ""
+    print "Isotopes entered into the program:", isotopes
